7bfaea930736b80561b83be006d232a4fe9d8b8a
Rosmery Torres <rosmery.torres10@gmail.com>
2024-01-30 19:24:42 -0300

    agrega componente card

--- src/components/CardGroup.tsx
+++ src/components/CardGroup.tsx
@@ -1,6 +1,6 @@
 export default function CardGroup({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex border-x border-solid border-[#262626]">
+    <div className="border-x border-solid border-[#262626] sm:block md:flex">
       {children}
     </div>
   );
