c75231a8666bd4c9fb643427f3893457184c752a
Enrique Bermúdez <38434726+enbermudas@users.noreply.github.com>
2024-10-09 18:15:07 -0300

    style: modify some tailwind classes and re-order imports

--- src/components/AboutStep/AboutStep.tsx
+++ src/components/AboutStep/AboutStep.tsx
@@ -6,18 +6,18 @@ export interface AboutProps {
 
 export default function AboutStep({ step, title, description }: AboutProps) {
   return (
-    <div className="flex h-[517px] flex-col gap-[50px] px-[50px] py-[100px] max-laptop:h-[452px] max-laptop:gap-[40px] max-laptop:px-40 max-laptop:py-80 max-mobile:h-[397px] max-mobile:gap-[30px] max-mobile:px-24 max-mobile:py-40">
-      <div className="flex h-[105px] items-center gap-3.5 max-laptop:h-[84px] max-mobile:h-[56px] max-mobile:gap-2.5">
-        <p className="text-[130px] font-semibold leading-[130px] max-laptop:text-[95px] max-mobile:text-[75px]">
+    <div className="flex h-[463px] flex-col gap-[50px] px-[50px] py-[100px] max-laptop:h-[402px] max-laptop:gap-10 max-laptop:px-40 max-laptop:py-80 max-mobile:h-[334px] max-mobile:gap-[30px] max-mobile:px-6 max-mobile:py-10">
+      <div className="flex h-[105px] items-center gap-3.5 max-laptop:h-[84px] max-mobile:h-14 max-mobile:gap-2.5">
+        <p className="text-[150px] font-semibold max-laptop:text-[120px] max-mobile:text-[80px]">
           {step}
         </p>
-        <span className="size-full border-b border-neutral-800 pl-2.5 pt-7">
-          <h1 className="text-[38px] font-semibold leading-[57px] max-laptop:text-[26px] max-laptop:leading-[39px] max-mobile:text-[22px] max-mobile:leading-[33px]">
+        <div className="flex size-full border-b border-neutral-800 pb-3.5">
+          <h1 className="ml-3.5 self-end text-3xl font-medium max-laptop:text-2xl max-mobile:text-xl">
             {title}
           </h1>
-        </span>
+        </div>
       </div>
-      <p className="h-[162px] text-[18px] leading-[27px] max-laptop:text-[16px] max-laptop:leading-[24px] max-mobile:text-[14px] max-mobile:leading-[21px]">
+      <p className="h-[135px] text-base max-laptop:h-[120px] max-mobile:h-[168px] max-mobile:text-sm">
         {description}
       </p>
     </div>
