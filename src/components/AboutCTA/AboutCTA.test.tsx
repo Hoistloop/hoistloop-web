10d7dc915cddc17538790a421073c56fb759e42a
andrea alvarado <andreassilva0108@gmail.com>
2025-01-18 15:49:32 -0300

    ajuste en test de AboutCTA

--- src/components/AboutCTA/AboutCTA.test.tsx
+++ src/components/AboutCTA/AboutCTA.test.tsx
@@ -1,21 +1,27 @@
 import { expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { PartialStoryFn, StoryContext } from "@storybook/csf";
 
+import withRouter from "@components/RouterWrapper";
 import AboutCTA from "./AboutCTA";
-
+const renderWithRouter = (
+  Story: PartialStoryFn,
+  context: StoryContext = { parameters: {} } as StoryContext,
+) => {
+  return render(withRouter(Story, context));
+};
 describe("components/StepCard", () => {
   test("render component", () => {
-    render(
+    renderWithRouter(() => (
       <AboutCTA
         imageUrl="https://place-hold.it/150x150"
         title="Today, SquareUp Continues to Thrive as a Leading Digital Product Agency....."
         description="Combining the power of design, engineering, and project management to create transformative digital experiences. They invite you to join them on their journey and discover how they can help bring your digital ideas to life."
         header="Welcome to Hoistloop"
         subHeader="Where collaboration, Expertise, and Client-Centricity Intersect to Shape the Future of Digital Innovation."
-        button={{ text: "Start Project" }}
-      />,
-    );
-
+        button={{ text: "Start Project", link: "/contact" }}
+      />
+    ));
     expect(screen.getByText(/Today, SquareUp Continues/i)).toBeDefined();
   });
 });
