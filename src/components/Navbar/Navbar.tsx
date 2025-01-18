7e7b73f9b3d0a6aecbbcbffbf8d2d525a53adf45
andrea alvarado <andreassilva0108@gmail.com>
2025-01-18 13:22:52 -0300

    ajuste a la prueba de navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -60,6 +60,7 @@ export default function Navbar() {
         <div className="sm:hidden">
           <button
             onClick={() => setOpen(!open)}
+            data-testid="mobile-menu-button"
             className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           >
             {open ? (
@@ -107,6 +108,7 @@ export default function Navbar() {
                 <Link
                   to={route.path}
                   key={route.name}
+                  data-testid={route.name}
                   className={`block rounded-md px-3 py-2 font-medium text-gray-300 hover:bg-gray-700 hover:text-white ${
                     router.location.pathname === route.path
                       ? "bg-gray-700 text-white"
